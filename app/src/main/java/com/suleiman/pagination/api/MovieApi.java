1d11fe152befd19200611b3a107270506ef35bf3
Richard <ric17101raguine@gmail.com>
2017-12-01 14:08:48 +0800

    *initial commit

---
+++ app/src/main/java/com/suleiman/pagination/api/MovieApi.java
@@ -0,0 +1,36 @@
+package com.suleiman.pagination.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Pagination
+ * Created by Suleiman19 on 10/27/16.
+ * Copyright (c) 2016. Suleiman Ali Shakir. All rights reserved.
+ */
+
+public class MovieApi {
+
+    private static Retrofit retrofit = null;
+
+    private static OkHttpClient buildClient() {
+        return new OkHttpClient
+                .Builder()
+                .addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build();
+    }
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .client(buildClient())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .build();
+        }
+        return retrofit;
+    }
+
+}
